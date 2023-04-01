a27b47e8178b8bf484607a97b1c5e917f8c801c9
Raagav Prasanna <raagav.pras@gmail.com>
2023-03-31 20:53:32 -0400

    fuck this

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -9,7 +9,7 @@ function NavBar({usersSelections}) {
         <nav className='NavElement'>
             <img src={coffee} alt="CoffeeLogo" id="coffeeImg"></img>
             <Link to="/" className="Link" id="LinkHome"> <b>OverFlow </b>  </Link>
-            <Link to="/Analysis" className="Link" state={{usersSelections: usersSelections}} id="LinkAnalysis"> Analysis </Link>
+            <Link to={{pathname: "/Analysis",state:{usersSelections: usersSelections} }} className="Link" id="LinkAnalysis"> Analysis </Link>
         </nav>
     </div>
   )
