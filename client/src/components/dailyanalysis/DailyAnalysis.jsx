9ea9f584972fc6b555f9d7cfbd94140d68a24b02
Matteo Robidoux <SDJ00008@gmail.com>
2023-03-31 21:50:15 -0400

    fuhrwf

--- client/src/components/dailyanalysis/DailyAnalysis.jsx
+++ client/src/components/dailyanalysis/DailyAnalysis.jsx
@@ -1,11 +1,37 @@
 import { useState } from 'react'
 import './DailyAnalysis.css'
 
-function DailyAnalysis() {
+function DailyAnalysis(props) {
+
+  function switchEmoji(){
+    switch(props.totalCaffeineConsumed){
+      case (props.totalCaffeineConsumed <= 300):
+        return "😴"
+      case (props.totalCaffeineConsumed <= 600 && props.totalCaffeineConsumed > 300):
+        return "😖"
+      case (props.totalCaffeineConsumed <= 900 && props.totalCaffeineConsumed > 600):
+        return "😁"
+      case (props.totalCaffeineConsumed <= 1200 && props.totalCaffeineConsumed > 900):
+        return "😵"
+      case (props.totalCaffeineConsumed > 1200):
+        return "☠️"
+      default:
+        return "☕"
+    }
+  }
 
   return (
     <div className="DailyAnalysis">
         <h2> Daily Analysis </h2>
+        <p> {switchEmoji()} </p>
+        <p> You have consumed: </p>
+        {props.usersSelections.map((product) => {
+          return (
+            <div className="Product">
+              <h3>{product.drink}</h3>
+            </div>
+          )
+        })}
     </div>
   )
 }
