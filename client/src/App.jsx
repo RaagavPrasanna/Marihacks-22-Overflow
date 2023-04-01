a5671214fdcfcca9c68e31c640357e1b2040c964
Raagav Prasanna <raagav.pras@gmail.com>
2023-03-31 20:19:35 -0400

    init search and selections

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import MainPage from "./MainPage.jsx"
 import Analysis from "./Analysis.jsx"
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
--- client/src/MainPage.jsx
+++ client/src/MainPage.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
+import SearchBar from "./SearchBar.jsx"
+import data from '../../server/data/data.js'
+import Selections from "./Selections.jsx"
 
 function MainPage() {
 
+  const [usersSelections, setUsersSelections] = useState([]);
+
+  const addProduct = (product) => {
+    const copy = [...usersSelections];
+    copy.push(product);
+    setUsersSelections([...copy]);
+  } 
+
+
   return (
-    <div className="MainPage">
-        <p>Main Page</p>
+    <div style={{display: "flex", alignItems: "center"}}>
+        <SearchBar addProduct={addProduct} placeholder="Enter product name" data={data} />
+        <Selections usersSelections={usersSelections}/>
     </div>
   )
 }
