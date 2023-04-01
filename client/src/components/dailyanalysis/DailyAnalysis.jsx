60128aa4c0203397d41a91ed6edd594d7c8f1a27
Matteo Robidoux <SDJ00008@gmail.com>
2023-03-31 21:01:32 -0400

    ded

---
+++ client/src/components/dailyanalysis/DailyAnalysis.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './DailyAnalysis.css'
+
+function DailyAnalysis() {
+
+  return (
+    <div className="DailyAnalysis">
+        <h2> Daily Analysis </h2>
+    </div>
+  )
+}
+
+export default DailyAnalysis
